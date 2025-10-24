7b844fd5eafb178bd9391e9a1ed43f9db23c7e45
Ignatius-code1 <igidydabbhs@gmail.com>
2025-10-24 13:13:56 +0300

    add app.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { AppNavigator } from './src/navigation/AppNavigator';
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="auto" />
+    <NavigationContainer>
       <AppNavigator />
-    </>
+    </NavigationContainer>
   );
 }
