ccefbadde37cd39eba1031f794e977009982c673
itskheila <munenetiara@gmail.com>
2025-10-26 06:07:37 +0300

    jwt authentication

---
+++ nexus_backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from .app.models.student_model import db
+from .app.auth import app as auth_app
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Register blueprints
+app.register_blueprint(auth_app)
+
+@app.cli.command('init-db')
+def init_db():
+    """Initialize the database."""
+    db.create_all()
+    print('Initialized the database.')
+
+if __name__ == '__main__':
+    app.run(debug=True)
