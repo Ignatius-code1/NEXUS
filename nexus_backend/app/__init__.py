1ab82044799fca58e749d78cb6372491d873accd
Ignatius-code1 <igidydabbhs@gmail.com>
2025-10-28 15:44:58 +0300

    WIP: Changes to init file before merging

--- nexus_backend/app/__init__.py
+++ nexus_backend/app/__init__.py
@@ -22,12 +22,17 @@ def create_app():
     jwt.init_app(app)
     
     # Import models (needed for migrations)
-    from app.models import user_model, password_reset, session_model
+    from app.models import user_model, password_reset, session_model, attendance
     
     # Register blueprints
     from app.routes.auth_routes import auth_bp
     from app.routes.admin_routes import admin_bp
+    from app.routes.attendant_routes import attendant_bp
+    from app.routes.attendee_routes import attendee_bp
+    
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(admin_bp, url_prefix='/api/admin')
+    app.register_blueprint(attendant_bp, url_prefix='/api/attendant')
+    app.register_blueprint(attendee_bp, url_prefix='/api/attendee')
     
     return app
